104c5df5af4c1078f5e398f237b8a68d007e3bfb
rahulc0dy <rahulchakraborty337@gmail.com>
2025-01-20 13:25:03 +0530

    Env example config

--- src/env.ts
+++ src/env.ts
@@ -18,20 +18,8 @@ export const { PORT, NODE_ENV } = createEnv({
    */
   runtimeEnv: process.env,
 
-  /**
-   * By default, this library will feed the environment variables directly to
-   * the Zod validator.
-   *
-   * This means that if you have an empty string for a value that is supposed
-   * to be a number (e.g. `PORT=` in a ".env" file), Zod will incorrectly flag
-   * it as a type mismatch violation. Additionally, if you have an empty string
-   * for a value that is supposed to be a string with a default value (e.g.
-   * `DOMAIN=` in an ".env" file), the default value will never be applied.
-   *
-   * In order to solve these issues, we recommend that all new projects
-   * explicitly specify this option as true.
-   */
   emptyStringAsUndefined: true,
+
   // Called when the schema validation fails.
   onValidationError: (error: ZodError) => {
     console.error(
