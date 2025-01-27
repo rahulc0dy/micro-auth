f8e4f6e49921a13f23324a3aff900a89e794ab2a
rahulc0dy <rahulchakraborty337@gmail.com>
2025-01-27 12:00:39 +0530

    feat(log-middleware): separate request logging middleware for dev environment.

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,27 @@
 import { Hono } from "hono";
 import { registerV1Routes } from "./routes/v1/routes.config.ts";
-import { reqLogMiddleware } from "./middlewares/requestLog.middleware.ts";
+import { logger } from "hono/logger";
 import errorHandler from "./utils/errorHandler.ts";
+import { reqLogMiddleware } from "./middlewares/requestLog.middleware.ts";
+import chalk from "chalk";
+import { APP_ENV } from "./env.ts";
 
 const app = new Hono();
 
-app.use(reqLogMiddleware);
+APP_ENV === "production"
+  ? app.use(reqLogMiddleware)
+  : app.use(logger((message) => {
+    console.log(
+      `${chalk.gray(`[${new Date().toISOString()}]`)} ${
+        chalk.bgMagentaBright.blackBright.bold.italic(" REQ LOG ")
+      } ${chalk.cyanBright("server")} ${message}`,
+    );
+  }));
+
+// Api version 1 routing.
 registerV1Routes(app);
 
+// Universal error handler.
 app.onError(errorHandler);
 
 export default app;
